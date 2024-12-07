8e9e159518077806e92ea42bf52e5767f5aeae60
Nukaus345 <nukaus032002@gmail.com>
2024-12-07 02:07:44 -0300

    Criando uma página de calculo de tempo de serviço

--- app.py
+++ app.py
@@ -12,19 +12,8 @@ def calculate():
     data_inicio = request.form['data_inicio']
     data_final = request.form['data_final']
    
-    separado_inicial = data_inicio.split("")
-    ano_inicial = int(separado_inicial [0])
-    mes_inicial = int(separado_inicial [1])
-    dia_inicial = int(separado_inicial [2])
-
-
-    separado_final = data_final.split("")
-    ano_final = int(separado_final [0])
-    mes_final = int(separado_final [1])
-    dia_final = int(separado_final [2])
-
-    data_inicio = datetime(dia_inicial, mes_inicial, ano_inicial)
-    data_final = datetime(dia_final, mes_final, ano_final)
+    data_inicio = datetime.strptime(data_inicio, '%Y-%m-%d')
+    data_final = datetime.strptime(data_final, '%Y-%m-%d')
 
     diferenca = data_final - data_inicio 
     dias_totais = diferenca.days
